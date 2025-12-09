6c6d036f7cb0f7276e1c9af914221d95698d18b2
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2025-12-09 19:18:41 +0100

    Enhance page object with req property from req.json
    Added a req property to the page object and initialized it with data from req.json.

--- pager.js
+++ pager.js
@@ -2,11 +2,16 @@ const fs = require('fs')
 
 const page = {
     content:"",
+    req: "",
     write:(c)=>{
         page.content+=c
     }
 }
 
+page.req = JSON.parse(fs.readFileSync('./core/req.json').toString())
+
+
 eval(fs.readFileSync(`./routes/${process.argv[2]}`).toString())
+
 
 console.log(page.content)
